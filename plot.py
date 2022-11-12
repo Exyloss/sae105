8959d8d3d50da5f4bf891182b99d6ea7b201444f
Exyloss <antonin.pons@protonmail.com>
2022-11-12 16:30:27 +0100

    commit

--- plot.py
+++ plot.py
@@ -1,12 +1,33 @@
 import matplotlib.pyplot as pyp
+import pandas as p
 import json
 
-f = open('browser.json')
+def get_hour(liste: list) -> list:
+    heures = []
+    for i in liste:
+        heures.append(int(i.split(":")[1:2][0]))
+    return heures
+
+f = open('heure.json')
 
 data = json.load(f)
 
 f.close()
 
+heure = get_hour(data)
+
+nb_heures = {}
+
+for i in heure:
+    nb_heures[i] = nb_heures.get(i, 0)+1
+
+df = p.DataFrame({'heures': nb_heures.keys(), 'Nombre de connections': nb_heures.values()})
+
+df.plot.bar(x='heures', y='Nombre de connections')
+
+pyp.show()
+
+"""
 keys = []
 values = []
 
@@ -21,3 +42,4 @@ pyp.pie(values, labels=keys, normalize=True)
 pyp.legend()
 
 pyp.show()
+"""
