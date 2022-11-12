1dddc01376035b02cfa461e17817b0c5a12b834d
Exyloss <antoninpons@protonmail.com>
2022-11-12 22:54:29 +0100

    commit

--- export_file.py
+++ export_file.py
@@ -1,5 +1,7 @@
 import json
 import csv
+import xml.etree.ElementTree as et
+import xml.dom.minidom
 
 def exportToCSVFile(liste: list, fichier: str) -> bool:
     try:
@@ -17,11 +19,21 @@ def exportToJSONFile(liste: list, fichier: str) -> bool:
     return True
 
 def exportToXMLFile(liste: list, fichier: str) -> bool:
-    try:
-        ...
-        return True
-    except:
-        return False
+    data = et.Element('root')
+    elts = []
+    for line in liste:
+        elts.append(et.SubElement(data, 'request'))
+        et.SubElement(elts[-1], 'ip').text        = line[0]
+        et.SubElement(elts[-1], 'date').text      = line[1]
+        et.SubElement(elts[-1], 'http_code').text = line[2]
+        et.SubElement(elts[-1], 'system').text    = line[3]
+        et.SubElement(elts[-1], 'browser').text   = line[4]
+
+    b_xml = et.tostring(data)
+    xmlstr = xml.dom.minidom.parseString(b_xml).toprettyxml()
+    with open(fichier, 'w') as f:
+        f.write(xmlstr)
+    return True
 
 def exportToText(liste: list, fichier: str) -> bool:
     try:
