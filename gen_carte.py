e2090aab30b875baa50073d8d1c672f73028bac9
Exyloss <antoninpons@protonmail.com>
2022-11-12 22:18:15 +0100

    commit

--- gen_carte.py
+++ gen_carte.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import folium
 import csv
+import json
 
 m = folium.Map(location=[50, 0], zoom_start=5)
 
-coord = []
 
+coord = []
 with open('coord.csv', mode ='r') as file:
   csvFile = csv.reader(file)
   for line in csvFile:
       if line not in coord:
         coord.append(line)
+
+
 
 for ip in coord:
     folium.Marker((ip[0], ip[1])).add_to(m)
